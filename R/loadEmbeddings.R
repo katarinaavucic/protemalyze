ccdf453afdd87bbf3731fb703ac081ac96592935
Katarina Vucic <kavucic@icloud.com>
2024-10-30 09:13:56 -0400

    Added eColiEmbeddingMatrix dataset with documentation and added examples to loadEmbeddings

--- R/loadEmbeddings.R
+++ R/loadEmbeddings.R
@@ -1,22 +1,23 @@
 #' Loads Embedding Matrices From Different File Types
 #'
-#' A function that loads an embedding matrix given a path
-#' from the root folder and file type that is either "csv",
-#' tsv", or "h5".
+#' A function that loads an embedding matrix given a path from the root folder
+#' and file type that is either "csv", tsv", or "h5".
 #'
-#' @param path A string containing the path to the
-#'     embedding matrix from the root folder. The embedding
-#'     matrix must have proteins in the rows with the
-#'     identifier in the index and embedding dimensions in
-#'     the columns.
+#' @param path A string containing the path to the embedding matrix from the
+#'     root folder. The embedding matrix must have proteins in the rows with the
+#'     identifier in the index and embedding dimensions in the columns.
 #'
-#' @param fileType A string specifying the file type of the
-#'     embedding matrix, must be either "csv", "tsv", or "h5".
+#' @param fileType A string specifying the file type of the embedding matrix,
+#'     must be either "csv", "tsv", or "h5".
 #'
-#' @return Returns a DataFrame representing the embedding
-#'     matrix with proteins in the rows and embedding
-#'     dimensions in the columns.
-#'
+#' @return Returns a data frame representing the embedding matrix with proteins
+#'     in the rows and embedding dimensions in the columns.
+#' @examples
+#' \dontrun{
+#'  loadEmbeddings("path/to/file.csv", "csv")
+#'  loadEmbeddings("path/to/file.tsv", "tsv")
+#'  loadEmbeddings("path/to/file.h5", "h5")
+#' }
 #' @export
 #' @importFrom readr read_csv read_tsv
 #' @importFrom rhdf5 h5read
