be546023a7200a6c3d8934ad37ac58761e0f86e0
Katarina Vucic <kavucic@icloud.com>
2024-11-26 21:50:17 -0500

    feat: finalized base functionality for shiny application

--- inst/shiny-scripts/app.R
+++ inst/shiny-scripts/app.R
@@ -2,8 +2,15 @@
 # https://shiny.posit.co/r/gallery/application-layout/tabsets/
 # file upload (include citation)
 # https://shiny.posit.co/r/gallery/widgets/file-upload/
+# fluid row (include citation)
+# https://shiny.posit.co/r/reference/shiny/1.8.0/fluidpage.html
+# DT (include citation)
+# https://shiny.posit.co/r/articles/build/datatables/
 
 library(shiny)
+library(plotly)
+library(DT)
+library(dplyr)
 
 # Set the upload limit to 50MB
 # source: https://mastering-shiny.org/action-transfer.html
@@ -25,11 +32,11 @@ ui <- fluidPage(
       fileInput("embeddingFile", "Input Embedding Matrix File",
                 accept = c(".csv", ".tsv", ".h5")),
 
-      # Input: File for Mapping (optional).
+      # Input file for Mapping (optional).
       fileInput("mappingFile", "Optional: Input Mapping File",
                 accept = c(".csv", ".tsv")),
 
-      # Action button to run the analysis
+      # Action button to run the analysis.
       actionButton(inputId = "runButton", label = "Run Analysis")
 
       # TODO: add inputs for specifying distance metrics, threads, etc.
@@ -40,12 +47,19 @@ ui <- fluidPage(
     mainPanel(
 
 
-      # Output: Tabset w/ UMAP, Pairs, Distributions, and Mapping ----
+      # Output tabset with UMAP, Pairs, and Mapping Analysis ----
       tabsetPanel(type = "tabs",
-                  tabPanel("Embedding UMAP", plotOutput("umapPlot")),
-                  tabPanel("Closest and Farthest Pairs", plotOutput("pairsTable")),
-                  tabPanel("Rank & Distance Distributions", plotOutput("distributionPlot")),
-                  tabPanel("Mapping Ranks & Distances", tableOutput("mappingTable"))
+                  tabPanel("Embedding UMAP", plotlyOutput("umapPlot")),
+                  tabPanel("Closest and Farthest Pairs", DTOutput("pairsTable")),
+                  tabPanel("Mapping Ranks & Distances", fluidRow(
+                    column(6,
+                           h4("Distance and Rank Table"),
+                           DTOutput("mappingTable")),
+                    column(6,
+                           h4("Distance Distribution"),
+                           plotOutput("distPlot", height = "300px"),
+                           h4("Rank Distribution"),
+                           plotOutput("rankPlot", height = "300px"))))
       )
     )
   )
@@ -54,7 +68,7 @@ ui <- fluidPage(
 # Define server logic for random distribution app ----
 server <- function(input, output) {
 
-  # Reactive expression to load and process embedding matrix when file is uploaded
+  # Reactive expression to load and process embedding matrix when file is uploaded.
   embeddingMatrix <- reactive({
     req(input$embeddingFile)
     # source: https://mastering-shiny.org/action-transfer.html
@@ -64,63 +78,78 @@ server <- function(input, output) {
     return(embeddingMatrix)
   })
 
-  # Reactive expression to process mapping file if uploaded
+  # Reactive expression to process mapping file if uploaded.
   paralogMapping <- reactive({
     req(input$mappingFile)
     mapping <- read.csv(input$mappingFile$datapath)
     return(mapping)
   })
 
-  # Run analysis when action button is clicked
+  # Run analysis when action button is clicked.
   observeEvent(input$runButton, {
 
-    # Generate UMAP plot using the processed embedding matrix.
-    output$umapPlot <- renderPlot({
-      # Must do this to use the result of the reactive expression.
-      embeddingMatrixData <- embeddingMatrix()
-      plot <- visualizeEmbeddingUMAP(embeddingMatrixData)
-      return(plot)
+    # Must do this to use the result of the reactive expression.
+    embeddingMatrixData <- embeddingMatrix()
+
+    # Render UMAP plot using the processed embedding matrix.
+    # This takes the longest but is on the first page so we will perform this
+    # as early as possible.
+    output$umapPlot <- renderPlotly({
+      visualizeEmbeddingUMAP(embeddingMatrixData)
     })
 
-    # Generate closest pairs and farthest pairs
-    output$pairsTable <- renderPlot({
-      # Must do this to use the result of the reactive expression.
-      embeddingMatrixData <- embeddingMatrix()
-      eColiDistMatrix <- generateDistanceMatrix(embeddingMatrixData)
-      eColiRankMatrix <- generateRankMatrix(eColiDistMatrix)
-      closestPairs <- getClosestPairs(eColiRankMatrix)
-      farthestPairs <- getFarthestPairs(eColiRankMatrix)
+    # Generate necessary tables for general analysis.
+    distMatrix <- generateDistanceMatrix(embeddingMatrixData)
+    rankMatrix <- generateRankMatrix(distMatrix)
+    closestPairs <- getClosestPairs(rankMatrix)
+    farthestPairs <- getFarthestPairs(rankMatrix)
 
-      # TODO: somehow return these tables.
+    # Merge closestPairs and farthestPairs on Protein.
+    combinedPairsTable <- dplyr::left_join(closestPairs, farthestPairs,
+                                           by = "Protein")
+
+    # Render the closest and farthest pairs table.
+    output$pairsTable <- renderDT({
+      return(datatable(combinedPairsTable,
+                       options = list(pageLength = 10,
+                                      lengthMenu = c(10, 20, 50, 100))))
     })
 
-    # Visualize rank and distance distributions
-    output$distributionPlot <- renderPlot({
-      # Must do this to use the result of the reactive expression.
-      embeddingMatrixData <- embeddingMatrix()
-      eColiDistMatrix <- generateDistanceMatrix(embeddingMatrixData)
-      eColiRankMatrix <- generateRankMatrix(eColiDistMatrix)
+    # Check if a mapping has been uploaded.
+    mapping <- paralogMapping()
 
-      distPlot <- visualizeDistanceDistribution(eColiDistMatrix)
-      rankPlot <- visualizeRankDistribution(eColiRankMatrix)
+    # If no mapping, send message to user.
+    if (is.null(mapping)) {
+      return(h3("Upload a mapping to view mapping-specific analysis."))
+    # If mapping, perform analysis.
+    } else {
+      # Generate necessary tables and plots for mapping analysis.
+      mappingDistances <- getDistancesByMapping(distMatrix, mapping)
+      mappingRanks <- getRanksByMapping(rankMatrix, mapping)
+      distPlot <- visualizeDistanceDistribution(distMatrix, mapping)
+      rankPlot <- visualizeRankDistribution(rankMatrix, mapping)
 
-      # TODO: somehow return these plots.
-    })
+      # Merge mappingDistances and mappingRanks on Protein1 and Protein2
+      combinedTable <- dplyr::left_join(mappingDistances, mappingRanks,
+                                        by = c("Protein1", "Protein2"))
 
-    # Show ranks and distances if mapping is provided
-    output$mappingTable <- renderTable({
-      mapping <- paralogMapping()
-      if (length(eColiMapping) > 0) {
-        eColiDistMatrix <- generateDistanceMatrix(embeddingMatrix())
-        eColiRankMatrix <- generateRankMatrix(eColiDistMatrix)
-        paralogDistances <- getDistancesByMapping(eColiDistMatrix, mapping)
-        paralogRanks <- getRanksByMapping(eColiRankMatrix, mapping)
-        distPlot <- visualizeDistanceDistribution(eColiDistMatrix)
-        rankPlot <- visualizeRankDistribution(eColiRankMatrix)
+      # Render the distance and rank table for the mapping.
+      output$mappingTable <- renderDT({
+        return(datatable(combinedTable,
+                         options = list(pageLength = 10,
+                                        lengthMenu = c(10, 20, 50, 100))))
+      })
 
-        # TODO: somehow return these tables and plots.
-      }
-    })
+      # Render distance distribution plot.
+      output$distPlot <- renderPlot({
+        return(visualizeDistanceDistribution(distMatrix, mapping))
+      })
+
+      # Render rank distribution plot.
+      output$rankPlot <- renderPlot({
+        return(visualizeRankDistribution(rankMatrix, mapping))
+      })
+    }
   })
 }
 
